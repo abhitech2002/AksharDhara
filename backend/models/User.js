26d2e36235bfd7f740959b6a0f8566d33a4a4e46
abhitech2002 <oabhishek31@gmail.com>
2025-05-19 16:45:19 +0530

    feat: add User model with schema for username, email, password, and role

---
+++ backend/models/User.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 3,
+        maxlength: 20,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /.+\@.+\..+/,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user',
+    },
+});
+
+export default mongoose.model('User', userSchema);
