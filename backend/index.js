fe7bc690973f1f5e26255dce4038be52edfea9c4
abhitech2002 <oabhishek31@gmail.com>
2025-05-19 16:45:45 +0530

    feat: add authentication routes for user registration and login

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { configDotenv } from 'dotenv';
 import connectDB from './config/db.js';
 import blogRoutes from './routes/blogRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 import cors from 'cors';
 
 configDotenv();
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1/blogs', blogRoutes);
+app.use('/api/v1/auth', authRoutes);
 
 connectDB().then(() => {
     app.listen(PORT, () => {
