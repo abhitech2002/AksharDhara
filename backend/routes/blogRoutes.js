e9a13780a500a892d9ee623dade43af5b05b5349
abhitech2002 <oabhishek31@gmail.com>
2025-05-20 15:17:06 +0530

    feat: add input validation for blog creation using Joi middleware

--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -7,10 +7,12 @@ import {
   deleteBlog,
 } from '../controllers/blogController.js';
 import { authenticate } from '../middlewares/authMiddleware.js';
+import {createBlogSchema} from "../validators/blogValidator.js";
+import { validate } from '../middlewares/validate.js';
 
 const router = express.Router();
 
-router.post('/', createBlog);
+router.post('/', verifyToken, validate(createBlogSchema), createBlog);
 router.get('/', getBlogs);
 router.get('/:id',authenticate, getBlogById);
 router.put('/:id',authenticate, updateBlog);
