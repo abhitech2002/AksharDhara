687d3fbd5e486f8ad311fa8e38e7f70389c2b677
abhitech2002 <oabhishek31@gmail.com>
2025-05-24 13:27:05 +0530

    feat(validation): add schema validation for auth and user routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { register, login } from '../controllers/authController.js';
+import { validate } from '../middlewares/validate.js';
+import { registerSchema, loginSchema, validatePassword } from "../validators/authValidator.js";
 
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', validate(registerSchema), validatePassword, register);
+router.post('/login', validate(loginSchema), login);
 
 export default router;
