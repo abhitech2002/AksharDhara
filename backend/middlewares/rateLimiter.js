7d39f326366019c40dbd6e5607fb98edfbe4d391
abhitech2002 <oabhishek31@gmail.com>
2025-05-24 21:24:18 +0530

    refactor(middleware): update logger import path in rateLimiter

--- backend/middlewares/rateLimiter.js
+++ backend/middlewares/rateLimiter.js
@@ -1,5 +1,5 @@
 import rateLimit from 'express-rate-limit';
-import { logger } from '../utils/logger.js';
+import { logger } from '../config/logger.js';
 
 const createLimiter = (options) => {
     return rateLimit({
