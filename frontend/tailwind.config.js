6dca9d56c636dfe56c93e886c4fe4778f18581b2
abhitech2002 <oabhishek31@gmail.com>
2025-05-24 11:39:20 +0530

    chore: downgrade React to v18.2.0 and add react-quill dependency

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,50 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            'code::before': {
+              content: '""'
+            },
+            'code::after': {
+              content: '""'
+            },
+            'blockquote p:first-of-type::before': {
+              content: '""'
+            },
+            'blockquote p:last-of-type::after': {
+              content: '""'
+            },
+            '.ql-align-center': {
+              textAlign: 'center',
+            },
+            '.ql-align-right': {
+              textAlign: 'right',
+            },
+            '.ql-align-justify': {
+              textAlign: 'justify',
+            },
+            img: {
+              marginTop: '1.5rem',
+              marginBottom: '1.5rem',
+            },
+            'ol > li::before': {
+              color: theme('colors.indigo.800'),
+            },
+            'ul > li::before': {
+              backgroundColor: theme('colors.indigo.800'),
+            },
+          },
+        },
+      }),
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
